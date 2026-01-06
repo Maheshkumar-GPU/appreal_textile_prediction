3f7ef18b5fd20a6c1c82374b9cbe3ec6835e67d7
Maheshkumar <tigermahesh6385@gmail.com>
2026-01-06 19:58:15 +0530

    day 1 work finished

--- main5.py
+++ main5.py
@@ -1,1 +1,38 @@
-import
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_df = pd.read_csv("C:/Users/Asus/OneDrive/Documents/ml project datasets/fashion apprealdataset/fashion-mnist_train.csv")
+test_df = pd.read_csv("C:/Users/Asus/OneDrive/Documents/ml project datasets/fashion apprealdataset/fashion-mnist_test.csv")
+print(train_df.head())
+print(train_df.shape)
+print(train_df.isnull().sum())
+print(train_df.describe())
+
+# splitting features
+x_train = train_df.iloc[:,1:].values
+y_train = train_df.iloc[:,0].values
+
+x_test = train_df.iloc[:,1:].values
+y_test = train_df.iloc[:,0].values
+
+# reshaping the images
+x_train = x_train.reshape(-1,28,28)
+x_test = x_test.reshape(-1,28,28)
+
+# normailzation
+x_train = x_train /255.0
+x_test = x_test /255.0
+
+# allocating class names
+class_names = ['t - shirt', 'trowser', 'pullover ', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+
+# vizualization
+
+plt.figure(figsize = (6,6))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(x_train[i],cmap='gray')
+    plt.title(class_names[y_train[i]])
+    plt.axis('off')
+plt.show()
