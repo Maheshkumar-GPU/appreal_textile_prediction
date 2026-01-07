4bd590ed5f4b7d155df2638e83fe838b339980f1
Maheshkumar <tigermahesh6385@gmail.com>
2026-01-07 21:59:02 +0530

    day 2 work completed

--- main5.py
+++ main5.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 train_df = pd.read_csv("C:/Users/Asus/OneDrive/Documents/ml project datasets/fashion apprealdataset/fashion-mnist_train.csv")
 test_df = pd.read_csv("C:/Users/Asus/OneDrive/Documents/ml project datasets/fashion apprealdataset/fashion-mnist_test.csv")
@@ -36,3 +38,22 @@ for i in range(9):
     plt.title(class_names[y_train[i]])
     plt.axis('off')
 plt.show()
+
+# adding channel dimension
+x_train = x_train.reshape(-1,28,28,1)
+x_test = x_test.reshape(-1,28,28,1)
+
+# one hot encoding label
+y_train_cato = to_categorical(y_train,num_classes=10)
+y_test_cato = to_categorical(y_test,num_classes=10)
+print("y_train encoded shape : ",y_train_cato.shape)
+print("y_test encoded shape : ",y_test_cato.shape)
+
+# creating a validation data
+x_train,x_val,y_train_cato, y_val_cato = train_test_split(x_train,y_train_cato,test_size=0.2,random_state=42)
+
+#shaping  checking
+print("data shapes ")
+print("training data ",x_train.shape,y_train_cato.shape)
+print("validation data  ",x_val.shape,y_val_cato.shape)
+print("testing data ",x_test.shape,y_test_cato.shape)
